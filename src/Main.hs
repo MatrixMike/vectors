b43d327cbf6b5f8a8426dcccfa92a8843c22b5c3
Mike <matroxmike@icloud.com>
2018-09-10 20:48:55 +1000

    creates a display

--- src/Main.hs
+++ src/Main.hs
@@ -4,5 +4,9 @@ module Main where
 import Vectors
 import Views
 
+--main :: IO ()
+--main = print "0k"
+
 main :: IO ()
-main = print "0k"
+main = Views.field        -- main = display window background axes
+--main = print "0k"
