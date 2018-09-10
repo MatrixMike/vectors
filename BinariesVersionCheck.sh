cebf88a42c99fcc423225923367d70cc29039b2b
Mike <matroxmike@icloud.com>
2018-09-10 20:18:56 +1000

    scripts

--- BinariesVersionCheck.sh
+++ BinariesVersionCheck.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ghc --version
 stack --version
 cabal --version
--- stackSeq.sh
+++ stackSeq.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 stack --version && stack clean && stack build && stack install && stack sdist && stack test && stack list-dependencies
 
 
